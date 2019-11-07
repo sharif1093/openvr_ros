79da147a53656f0b02c866a0609dfe123e1f9285
mrsharif <sharif1093@gmail.com>
2019-11-06 19:28:41 -0500

    Reduced the publishing rate

--- openvr_ros/src/track_publisher.cpp
+++ openvr_ros/src/track_publisher.cpp
@@ -5,12 +5,12 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "openvr_ros");
   ros::NodeHandle n("~");
-  ros::Publisher tracked_device_pose_publisher = n.advertise<openvr_ros::TrackedDevicePose>("tracked_device_pose", 1200);
+  ros::Publisher tracked_device_pose_publisher = n.advertise<openvr_ros::TrackedDevicePose>("tracked_device_pose", 300);
 
   vr::IVRSystem* vr_pointer = NULL;
   vr_pointer = openvr::initialize();
 
-  ros::Rate loop_rate(1000);
+  ros::Rate loop_rate(100);
   while (ros::ok())
   {
     openvr::PollEvents(vr_pointer);
